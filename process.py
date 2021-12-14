218f9d058beb6d73f4d11feeb37dd0ca3b44c90c
factormrp <factormrp@gmail.com>
2021-12-13 22:32:48 -0800

    WIP adding move attributes

--- process.py
+++ process.py
@@ -13,37 +13,73 @@ GRADEBOOK = ["5","5+","6A","6A+","6B","6B+","6C","6C+","7A","7A+","7B","7B+","7C
 # FUNCTIONS #
 #############
 
-def extract_move_string_data(moves):
+def extract_board_data(board_type):
+    """
+    Extracts relevant data for a specific hold
+    Return: dictionary
+    """
+    # define path to corresponding board holdsetup
+    holdpath = os.path.join("data","-".join(board_type.split())+"-hold-setup.json")
+
+    # import json from file path
+    holddata = core.import_json(holdpath)
+
+    # Convert data into a single dictionary of holds
+    holddict = {}
+    for holdset in holddata:
+        for hold in holdset["Holds"]:
+            key = hold["Location"]["Description"]
+            data = {
+                "X":hold["Location"]["X"],
+                "Y":hold["Location"]["Y"],
+                "Rotation":hold["Location"]["Rotation"],
+                "Direction":hold["Location"]["Direction"]
+            }
+            holddict[key] = data
+    return holddict
+    
+def extract_move_string_data(moves,board_type):
     """
     Extracts moves from a string and looks up their attributes
     Return: 
     """
-    # implement
-    pass
+    # get holdsetup for given board type
+    holdsetup = extract_board_data(board_type)
 
-def transform_data(filedata):
+    # initialize X, Y, Rotation, and Direction strings
+    X,Y,R,D = "","","",""
+
+    # for each move, get corresponding data
+    for move in moves.split(","):
+        X = X + "," + holdsetup[move]["X"]
+        Y = Y + "," + holdsetup[move]["Y"]
+        R = R + "," + holdsetup[move]["R"]
+        D = D + "," + holdsetup[move]["D"]
+
+    
+
+
+def transform_problem_data(filedata):
     """
     Transforms data from a json file to a dataframe of values
     Return: DataFrame
     """
     # initialize master dataframe
-    df = pd.DataFrame(columns=["ROUTE_ID","ROUTE_NAME","ROUTE_MOVES","ROUTE_START","ROUTE_END","ROUTE_GRADE","IS_BENCHMARK","REPEATS","RATING","BOARD_TYPE","BOARD_ANGLE"])
+    df = pd.DataFrame(columns=["ROUTE_MOVES","ROUTE_START","ROUTE_END","MOVE_ANGLES","ROUTE_GRADE","IS_BENCHMARK","REPEATS","RATING","BOARD_ANGLE","BOARD_TYPE"])
 
-    # for each problem in file, extract infso 
+    # for each problem in file, extract info
     for i,problem in enumerate(filedata.values()):
         problem = core.convert_to_simple_namespace(problem)
         entry = []
-        entry.append(problem.Id)
-        entry.append(problem.NameForUrl)
         entry.append(",".join([a["Description"] for a in problem.Moves]))
         entry.append(",".join([a["Description"] for a in problem.Moves if a.get("IsStart")]))
         entry.append(",".join([a["Description"] for a in problem.Moves if a.get("IsEnd")]))
-        entry.append(",".join(extract_move_string_data(entry[2])))
+        entry.append(",".join(extract_move_string_data(entry[2],problem.Holdsetup["Description"].lower())))
         entry.append(GRADEBOOK.index(problem.Grade))
         entry.append(problem.IsBenchmark)
         entry.append(problem.Repeats)
         entry.append(problem.UserRating)
-        entry.append(problem.Holdsetup["Description"].lower())
+        entry.append()
         entry.append(int(problem.MoonBoardConfiguration["Description"][:2]))
         df.loc[i,:] = entry
     return df
@@ -67,7 +103,7 @@ def extract_data(directory_path):
                 filedata = core.import_json(path)
 
                 # Convert json to DataFrame
-                filedf = transform_data(filedata)
+                filedf = transform_problem_data(filedata)
 
                 df = df.append(filedf)
     return df
